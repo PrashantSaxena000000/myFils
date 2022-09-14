eab4812b74280384035474d08ed423b095a057a6
PrashantSaxena000000 <prashantsaxena00000@gmail.com>
2022-09-14 21:30:01 +0530

    v2

--- script.js
+++ script.js
@@ -65,3 +65,30 @@ function uploadFile(name){
   let data = new FormData(form); //FormData is an object to easily send form data
   xhr.send(data); //sending form data
 }
+
+const fileTypes = [
+  "image/apng",
+  "image/bmp",
+  "image/gif",
+  "image/jpeg",
+  "image/pjpeg",
+  "image/png",
+  "image/svg+xml",
+  "image/tiff",
+  "image/webp",
+  "image/x-icon"
+];
+
+function validFileType(file) {
+  return fileTypes.includes(file.type);
+}
+
+function returnFileSize(number) {
+  if (number < 1024) {
+    return `${number} bytes`;
+  } else if (number >= 1024 && number < 1048576) {
+    return `${(number / 1024).toFixed(1)} KB`;
+  } else if (number >= 1048576) {
+    return `${(number / 1048576).toFixed(1)} MB`;
+  }
+}
